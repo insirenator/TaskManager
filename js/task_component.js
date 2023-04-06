ba6cdf77dcb5d080c9f7adb3bded539817df3793
insirenator <arsshakeeb149@protonmail.com>
2023-04-06 12:51:16 +0530

    Fixed: task editing on done and close

--- js/task_component.js
+++ js/task_component.js
@@ -1,5 +1,7 @@
 function createTaskComponent(taskName) {
-	
+
+	let task_name = taskName;
+
 	// Main container
 	const taskContainer = document.createElement('div');
 	taskContainer.classList.add('task-container');
@@ -16,9 +18,13 @@ function createTaskComponent(taskName) {
 	task.type = "text";
 	task.disabled = true;
 	
-	task.onblur = () => {
+	/*task.onblur = () => {
 		task.disabled = true;
-	};
+		editBtn.style.display = "inline";
+		deleteBtn.style.display = "inline";
+		checkbox.style.display = "inline";		
+		doneBtn.style.display = "none";
+	};*/
 	
 	// Control Icons
 	// Edit
@@ -26,10 +32,7 @@ function createTaskComponent(taskName) {
 	editBtn.classList.add('material-icons');
 	editBtn.classList.add('icons');
 	editBtn.textContent = 'edit';
-	editBtn.addEventListener('click', () => {
-		task.disabled = false;
-		task.focus();
-	});
+
 	//Delete
 	const deleteBtn = document.createElement('span');
 	deleteBtn.classList.add('material-icons');
@@ -39,11 +42,27 @@ function createTaskComponent(taskName) {
 		taskContainer.remove();
 	};
 
+	//Cross
+	const crossBtn = document.createElement('span');
+	crossBtn.classList.add('material-icons');
+	crossBtn.classList.add('icons');
+	crossBtn.textContent = 'close';
+	crossBtn.style.display = "none";
+
+	// Done 
+	const doneBtn = document.createElement('span');
+	doneBtn.classList.add('material-icons');
+	doneBtn.classList.add('icons');
+	doneBtn.textContent = 'done';
+	doneBtn.style.display = "none";
+
 	// Add all to main container
 	taskContainer.appendChild(checkbox);
 	taskContainer.appendChild(task);
 	taskContainer.appendChild(editBtn);
 	taskContainer.appendChild(deleteBtn);
+	taskContainer.appendChild(doneBtn);
+	taskContainer.appendChild(crossBtn);
 
 	//CheckBox Function
 	checkbox.onclick = () => {
@@ -53,6 +72,39 @@ function createTaskComponent(taskName) {
 			task.style.textDecoration = "none";
 		}
 	};
+
+	// Edit Button Function
+	editBtn.addEventListener('click', () => {
+		task.disabled = false;
+		task.focus();
+		editBtn.style.display = "none";
+		deleteBtn.style.display = "none";
+		checkbox.style.display = "none";
+		crossBtn.style.display = "inline";
+		doneBtn.style.display = "inline";
+	});
+
+	// Done Button Function
+	doneBtn.addEventListener('click', () => {
+		editBtn.style.display = "inline";
+		deleteBtn.style.display = "inline";
+		checkbox.style.display = "inline";
+		crossBtn.style.display = "none";		
+		doneBtn.style.display = "none";
+
+		task_name = task.value;
+
+	});
+
+	// Cross Button Function
+	crossBtn.addEventListener('click', () => {
+		editBtn.style.display = "inline";
+		deleteBtn.style.display = "inline";
+		checkbox.style.display = "inline";
+		crossBtn.style.display = "none";		
+		doneBtn.style.display = "none";
+		task.value = task_name;
+	});
 
 	//Return component
 	return taskContainer;
