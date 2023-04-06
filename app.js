96ca7c21aa807bf8e6b5e8bca7a20acdb2e69aac
insirenator <arsshakeeb149@protonmail.com>
2023-04-06 10:22:06 +0530

    Base Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import { createTaskComponent} from './js/task_component.js';
+
+// const newTask = createTaskComponent("Do the laundry.");
+
+// document.querySelector('.root-container').appendChild(newTask);
+
+const addBtn = document.querySelector('#add-btn');
+const taskInput = document.querySelector('#task-input');
+const displayArea = document.querySelector('.display-area');
+
+addBtn.addEventListener('click', () => {
+	const taskName = taskInput.value;
+
+	if(taskName) {
+		const newTask = createTaskComponent(taskName);
+		displayArea.appendChild(newTask);
+
+	}
+});
---
+++ js/task_component.js
@@ -0,0 +1,44 @@
+function createTaskComponent(taskName) {
+	// Main container
+	const taskContainer = document.createElement('div');
+	//Checkbox
+	const checkbox = document.createElement('input');
+	checkbox.type = "checkbox";
+	// Task
+	const task = document.createElement('input');
+	task.value = taskName;
+	task.type = "text";
+	task.disabled = true;
+	task.onblur = () => {
+		task.disabled = true;
+	};
+	// Control Icons
+	// Edit
+	const editBtn = document.createElement('span');
+	editBtn.classList.add('material-icons');
+	editBtn.classList.add('icons');
+	editBtn.textContent = 'edit';
+	editBtn.addEventListener('click', () => {
+		task.disabled = false;
+		task.focus();
+	});
+	//Delete
+	const deleteBtn = document.createElement('span');
+	deleteBtn.classList.add('material-icons');
+	deleteBtn.classList.add('icons');
+	deleteBtn.textContent = 'delete';
+	deleteBtn.onclick = () => {
+		taskContainer.remove();
+	};
+
+	// Add all to main container
+	taskContainer.appendChild(checkbox);
+	taskContainer.appendChild(task);
+	taskContainer.appendChild(editBtn);
+	taskContainer.appendChild(deleteBtn);
+
+	//Return component
+	return taskContainer;
+}
+
+export { createTaskComponent };
